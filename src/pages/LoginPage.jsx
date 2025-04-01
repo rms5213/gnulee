10fb131414b9ab5be92e594b5ca80cc229ea3c51
gnulee <gamebdy@naver.com>
2025-04-01 17:17:25 +0900

    [ADD]#03 개인과제[Frontend] 로그인 화면 구성하기
    #01 LoginPage 의 InputContaier 를
    #02 TextField 의 언더라인으로 수정
    
    ###
    # [ADD] : 기능이 추가되었을 때 사용
    # [CHANGE] : 기능이 변경되었을 때 사용
    # [REFACTOR] : 기능은 그대로지만, 소스 코드를 리팩토링 했을 떄 사용
    # [FIX] : 버그를 수정했을 때 사용
    #
    # 제목과 한줄 띄고 본문을 작성해 주세요.
    ###

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,32 +1,34 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import TextField from "../comp/TextField";
 import "./LoginPage.css";
-import SendIcon from "../assets/Vector.svg";
 
 const LoginPage = () => {
+    const navigate = useNavigate();
+
+    const handleSubmit = (name) => {
+        if (name) {
+            // console.log(` ${name}`);
+            navigate("/home");
+        }
+    };
+
     return (
         <div className="login-container">
-            <h1 className="login-title">Welcome Newbie!!</h1>
-            <h1 className="login-title">MyTodo makes it easy to stay organized and manage your life.</h1>
-
-            {/*<p className="login-description">*/}
-            {/*    MyTodo makes it easy to stay organized and manage your life.*/}
-            {/*</p>*/}
-            <form className="login-form">
-                <label htmlFor="name" className="login-label">
-                    What is your name?
-                </label>
-                <div className="input-container">
-                    <input
-                        id="name"
-                        type="text"
-                        placeholder="Input your name"
-                        className="login-input"
-                    />
-                    <button className="logo-button" onClick={() => navigate("/login")}>
-                        <img src={SendIcon} alt="전송하기" className="header-logo"/>
-                    </button>
-                </div>
-            </form>
+            <p className="login-title">Welcome Newbie!!</p>
+            <p className="login-description">
+                MyTodo makes it easy to stay organized and manage your life.
+            </p>
+            <label htmlFor="name" className="login-label">
+                What is your name?
+            </label>
+            <div className="input-container">
+                <TextField
+                    placeholder="Input your name"
+                    borderStyle={false}
+                    onSubmit={handleSubmit}
+                />
+            </div>
         </div>
     );
 };
